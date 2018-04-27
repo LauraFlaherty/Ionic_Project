1bad1747fe20e7a0891f7183da86144d3c8c1c1d
LauraFlaherty <g00348586@gmit.ie>
2018-04-27 14:27:20 +0100

    save

--- g00348586/src/app/app.module.ts
+++ g00348586/src/app/app.module.ts
@@ -6,11 +6,17 @@ import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { HolidayDestPage } from '../pages/holiday-dest/holiday-dest';
+import { IndoorPage } from '../pages/indoor/indoor';
+import { WeatherPage } from '../pages/weather/weather';
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,
+    HolidayDestPage, 
+    IndoorPage, 
+    WeatherPage
   ],
   imports: [
     BrowserModule,
@@ -19,7 +25,10 @@ import { HomePage } from '../pages/home/home';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    HomePage,
+    HolidayDestPage, 
+    IndoorPage, 
+    WeatherPage
   ],
   providers: [
     StatusBar,
--- g00348586/src/pages/home/home.ts
+++ g00348586/src/pages/home/home.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { HolidayDestPage } from './../holiday-dest/holiday-dest';
+import { IndoorPage } from './../indoor/indoor';
+import { WeatherPage } from './../weather/weather';
+
 
 @Component({
   selector: 'page-home',
@@ -10,5 +14,18 @@ export class HomePage {
   constructor(public navCtrl: NavController) {
 
   }
+ // open indoor page
+  openIndoor() {
+    this.navCtrl.push(IndoorPage);
+    }
+  // open weather page
+  openWeather() {
+    this.navCtrl.push(WeatherPage);
+    }
+  //open holiday destination
+  openHolidayDest() {
+    this.navCtrl.push(HolidayDestPage);
+    }
 
+    
 }
